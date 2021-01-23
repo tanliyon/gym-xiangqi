2da17ada4dc75c1e3472ad14a2177b6079706c7c
tanliyon <tanliyon@hotmail.com>
2021-01-22 23:26:58 -0500

    Set up repo

---
+++ gym-xiangqi/__init__.py
@@ -0,0 +1,6 @@
+from gym.envs.registration import register
+
+register(
+	id='xiangqi-v0'.
+	entry_point='gym_xiangqi.envs:XiangQiEnv',
+)
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+	name='gym_xiangqi',
+	version='0.0.1',
+	install_requites=['gym']
+)
