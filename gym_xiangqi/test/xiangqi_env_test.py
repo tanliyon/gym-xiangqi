660b404e95cd3b5d32f1ecdacda9e06b056aca7e
hojoungjang <jang88@purdue.edu>
2021-02-25 23:46:41 -0500

    Add test for conversion between move and action

--- gym_xiangqi/test/xiangqi_env_test.py
+++ gym_xiangqi/test/xiangqi_env_test.py
@@ -17,6 +17,44 @@ class TestXiangQiEnv(unittest.TestCase):
         self.assertIsNotNone(env.state)
         self.assertIsNotNone(env.observation_space)
         self.assertIsNotNone(env.action_space)
+        self.assertIsNotNone(env.possible_actions)
+
+    def test_move_to_action_space(self):
+        env = XiangQiEnv()
+        i = 0
+
+        for pid in range(1, 17):
+            for r1 in range(10):
+                for c1 in range(9):
+                    for r2 in range(10):
+                        for c2 in range(9):
+                            action_idx = env.move_to_action_space(
+                                pid, (r1, c1), (r2, c2)
+                            )
+                            self.assertEqual(action_idx, i)
+                            i += 1
+
+    def test_action_space_to_move(self):
+        def info_gen():
+            for piece in range(1, 17):
+                for r1 in range(10):
+                    for c1 in range(9):
+                        for r2 in range(10):
+                            for c2 in range(9):
+                                yield piece, (r1, c1), (r2, c2)
+
+        env = XiangQiEnv()
+        gen = info_gen()
+        n = pow(10 * 9, 2) * 16
+
+        for i in range(n):
+            pid, start, end = env.action_space_to_move(i)
+            ans_pid, ans_start, ans_end = next(gen)
+            self.assertEqual(pid, ans_pid)
+            self.assertEqual(start[0], ans_start[0])
+            self.assertEqual(start[1], ans_start[1])
+            self.assertEqual(end[0], ans_end[0])
+            self.assertEqual(end[1], ans_end[1])
 
 
 if __name__ == "__main__":
