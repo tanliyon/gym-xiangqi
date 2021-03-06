212c83dfe6bd2042d503a860da7308d3e329354a
Hojoung Jang <jang88@purdue.edu>
2021-03-06 13:14:19 -0500

    Break up env step test

--- gym_xiangqi/test/xiangqi_env_test.py
+++ gym_xiangqi/test/xiangqi_env_test.py
@@ -12,32 +12,37 @@ from gym_xiangqi.constants import (
 
 class TestXiangQiEnv(unittest.TestCase):
 
+    def setUp(self):
+        self.env = XiangQiEnv()
+
     def test_env_initialization(self):
-        env = XiangQiEnv(agent_color=RED)
-        self.assertEqual(env.agent_color, RED)
-        self.assertEqual(env.enemy_color, BLACK)
+        self.assertEqual(self.env.agent_color, RED)
+        self.assertEqual(self.env.enemy_color, BLACK)
 
-        env = XiangQiEnv(agent_color=BLACK)
-        self.assertEqual(env.agent_color, BLACK)
-        self.assertEqual(env.enemy_color, RED)
+        self.env = XiangQiEnv(agent_color=BLACK)
+        self.assertEqual(self.env.agent_color, BLACK)
+        self.assertEqual(self.env.enemy_color, RED)
 
-        self.assertIsNotNone(env.game)
-        self.assertIsNotNone(env.state)
-        self.assertIsNotNone(env.observation_space)
-        self.assertIsNotNone(env.action_space)
-        self.assertIsNotNone(env.agent_actions)
-        self.assertIsNotNone(env.enemy_actions)
+        self.assertIsNotNone(self.env.game)
+        self.assertIsNotNone(self.env.state)
+        self.assertIsNotNone(self.env.observation_space)
+        self.assertIsNotNone(self.env.action_space)
+        self.assertIsNotNone(self.env.agent_actions)
+        self.assertIsNotNone(self.env.enemy_actions)
 
-    def test_env_step(self):
-        env = XiangQiEnv()
-
-        # verify action input
+    def test_env_step_invalid_action(self):
+        """
+        verify action input
+        """
         with self.assertRaises(AssertionError):
-            env.step(-1)
+            self.env.step(-1)
 
-        # verify agent with illegal move
-        obs = env.state
-        new_obs, reward, done, info = env.step(0)
+    def test_env_step_illegal_move(self):
+        """
+        verify agent with illegal move
+        """
+        obs = self.env.state
+        new_obs, reward, done, info = self.env.step(0)
 
         for i in range(BOARD_ROWS):
             for j in range(BOARD_COLS):
@@ -45,33 +50,54 @@ class TestXiangQiEnv(unittest.TestCase):
         self.assertEqual(reward, ILLEGAL_MOVE)
         self.assertEqual(done, False)
 
-        # verify one round of agent and enemy turns
+    def test_env_step_one_round(self):
+        """
+        verify one round of agent and enemy turns
+        78727: Agent CANNON_1 (7, 1) -> (7, 4)
+        75172: Enemy CANNON_1 (2, 7) -> (2, 4)
+        """
         actions = [(78727, CANNON_1, (7, 1), (7, 4), 0, ENEMY),
                    (75172, CANNON_1, (2, 7), (2, 4), 0, AGENT)]
 
         for action, pid, (r1, c1), (r2, c2), points, next_turn in actions:
-            obs, reward, done, info = env.step(action)
+            obs, reward, done, info = self.env.step(action)
             self.assertEqual(obs[r1][c1], EMPTY)
-            self.assertEqual(obs[r2][c2], pid * env.turn * -1)
+            self.assertEqual(obs[r2][c2], pid * self.env.turn * -1)
             self.assertEqual(reward, points)
             self.assertEqual(done, False)
-            self.assertEqual(env.turn, next_turn)
+            self.assertEqual(self.env.turn, next_turn)
 
-        # verify points reward when taking opponent's piece
-        action = 78961      # Agent CANNON_1 (7, 4) -> (3, 4)
-        obs, reward, done, info = env.step(action)
+    def test_env_step_reward(self):
+        """
+        Simulates Agent (red) cannon taking a black soldier
+        78727: Agent CANNON_1 (7, 1) -> (7, 4)
+        75172: Enemy CANNON_1 (2, 7) -> (2, 4)
+        78961: Agent CANNON_1 (7, 4) -> (3, 4)
+        """
+        actions = [78727, 75172, 78961]
+        for action in actions:
+            obs, reward, done, info = self.env.step(action)
         self.assertEqual(obs[7][4], EMPTY)
         self.assertEqual(obs[3][4], CANNON_1)
         self.assertEqual(reward, PIECE_POINTS[SOLDIER_3])
         self.assertEqual(done, False)
-        self.assertEqual(env.enemy_piece[SOLDIER_3].state, DEAD)
+        self.assertEqual(self.env.enemy_piece[SOLDIER_3].state, DEAD)
 
-        # verify environment termination due to death of black general
-        for action in [123966, 75694]:  # first move has no particular meaning
-            obs, reward, done, info = env.step(action)
+    def test_env_step_done(self):
+        """
+        verify environment termination due to death of black general
+        78727: Agent CANNON_1 (7, 1) -> (7, 4)
+        75172: Enemy CANNON_1 (2, 7) -> (2, 4)
+        78961: Agent CANNON_1 (7, 4) -> (3, 4)
+        123966: Enemy SOLDIER_5 (3, 0) -> (4, 0)
+        75694: Agent CANNON_1 (3, 4) -> (0, 4) -- takes black general
+        """
+        actions = [78727, 75172, 78961, 123966, 75694]
+        for action in actions:
+            obs, reward, done, info = self.env.step(action)
         self.assertEqual(done, True)
         self.assertEqual(reward, PIECE_POINTS[GENERAL])
-        self.assertEqual(env.enemy_piece[GENERAL].state, DEAD)
+        self.assertEqual(self.env.enemy_piece[GENERAL].state, DEAD)
 
 
 if __name__ == "__main__":
