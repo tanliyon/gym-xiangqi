be8d695f724cf55d446c50cadb9be1aa4fe22dd7
tanliyon <tanliyon@hotmail.com>
2021-03-14 00:35:20 -0500

    Add max round for agent_env test

--- test/agent_env_test.py
+++ test/agent_env_test.py
@@ -3,6 +3,8 @@ from agents.random_agent import RandomAgent
 import gym
 
 
+MAX_ROUNDS = 500
+
 def test_random_agent_play_itself():
     """
     Test integration between agent and env by
@@ -12,7 +14,11 @@ def test_random_agent_play_itself():
     agent = RandomAgent()
 
     done = False
+    round_count = 0
     while not done:
         action = agent.move(env)
         _, _, done, _ = env.step(action)
+        round_count += 1
+        if round_count > MAX_ROUNDS:
+        	break
     env.close()
