0e84df941bb3c23dc25ab65be95a04a874f1c5de
Dongsuk Ma <kim1974@purdue.edu>
2021-03-21 13:31:45 -0400

    game sounds added

---
+++ gym_xiangqi/sound.py
@@ -0,0 +1,32 @@
+import pygame
+from gym_xiangqi.constants import PATH_TO_SOUNDS
+
+
+class Sound:
+    """
+    A class for sounds
+    """
+    def __init__(self):
+
+        # declare variables
+        self.bgm = None
+        self.piece_move = None
+
+        # init pygame mixer
+        pygame.init()
+        pygame.mixer.init()
+
+        # load sound effet for piece movements
+        self.load_piece_move()
+
+    # load background music
+    def play_bgm(self):
+        filename = "bgm.wav"
+        pygame.mixer.music.load(PATH_TO_SOUNDS + filename)
+        pygame.mixer.music.play(-1)
+
+    # load the sound effect for piece movements
+    def load_piece_move(self):
+        filename = "piece_move.wav"
+        target_file = PATH_TO_SOUNDS + filename
+        self.piece_move = pygame.mixer.Sound(target_file)
